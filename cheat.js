f73e92ca0e30d91ba5af4c24ee02aee4be30d608
mbranagan <melissajbranagan@gmail.com>
2019-05-04 22:14:22 -0700

    more adjusting formatting

--- cheat.js
+++ cheat.js
@@ -1,14 +1,14 @@
 // cheat program when hash already known
 
 let userRequest = new XMLHttpRequest();
-const userUrl='http://127.0.0.1:8888/users';
-userRequest.open("GET",userUrl);
-userRequest.setRequestHeader("X-Request-Checksum","B1F3387EB52D9C706B8749BAB5C413553990A2DB6D14C8D3EF7F4CCF302E6AF6");
+const userUrl = 'http://127.0.0.1:8888/users';
+userRequest.open("GET", userUrl);
+userRequest.setRequestHeader("X-Request-Checksum", "B1F3387EB52D9C706B8749BAB5C413553990A2DB6D14C8D3EF7F4CCF302E6AF6");
 userRequest.send();
 
-userRequest.onreadystatechange=function(){
-    if (this.readyState==4) {
-        if (this.status==200) {
+userRequest.onreadystatechange = function(){
+    if (this.readyState == 4) {
+        if (this.status == 200) {
             //console.log(userRequest.responseText);
             console.log(JSON.stringify(userRequest.responseText.split(`\n`)));
         }
