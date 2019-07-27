42b2bb7f2a66830c158e4c190b90abb11ce71425
mbranagan <melissajbranagan@gmail.com>
2019-07-27 14:36:48 -0700

    replacing XMLHttpRequest with fetch

--- constants.js
+++ constants.js
@@ -1,14 +1,14 @@
 // Constants used in server.js
 
-const url = 'http://127.0.0.1:8888';
 const authPath = '/auth';
 const userPath = '/users';
 const maxFailAttempts = 3;
 const badsecTokenHeader = 'Badsec-Authentication-Token';
 const checksumTokenHeader = 'X-Request-Checksum';
+const baseUrl = 'http://127.0.0.1:8888';
 
 module.exports = {
-    url: url,
+    baseUrl: baseUrl,
     authPath: authPath,
     userPath: userPath,
     maxFailAttempts: maxFailAttempts,
--- server.js
+++ server.js
@@ -1,53 +1,64 @@
 // Retrieve NOCList, output JSON object with user numbers
 
-var express = require('express');
 var crypto = require('crypto');
-var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+var fetch = require('node-fetch');
 var constant = require('./constants.js');
 
-const sendRequest = (path, headers) => {
-  let url = constant.url + path;
-  return recurMakeRequest(url, headers, 0)
+async function sendRequest(path, headers) {
+  let url = constant.baseUrl + path;
+  let value = await recurMakeRequest(url, headers, 0);
+  return value;
 }
 
-const recurMakeRequest = (url, headers, failCount) => {
+async function recurMakeRequest(url, headers, failCount) {
+
   if (failCount == constant.maxFailAttempts) {
     return false;
   }
-
-  let request = new XMLHttpRequest();
-  request.open("GET", url, false);
-
-  Object.entries(headers).forEach(([key, value]) => {
-    request.setRequestHeader(key, value);
-  });
-
-  request.send();
-  if (request.status == 200) {
-    return request;
-  }
-  recurMakeRequest(url, ++failCount);
+  let value = await fetch(url, {
+    method: 'GET',
+    headers: headers
+  }).then(
+      response => {
+      if (response.ok) {
+        return response;
+      }
+      else {
+        recurMakeRequest(url, ++failCount);
+      }
+    })
+  .catch ( err => {recurMakeRequest(url, ++failCount)})
+  return value;
 }
 
-const main = () => {
-    let authRequest = sendRequest(constant.authPath, {});
-    if (!authRequest) {
-      console.error("Reached max fail attempts.")
-      process.exit(1)
-    }
+async function main() {
+  let authRequest = await sendRequest(constant.authPath, {});
 
-    let authorizationToken = authRequest.getResponseHeader(constant.badsecTokenHeader);
-    let digestSha256 = crypto.createHash(`sha256`).update(authorizationToken + constant.userPath).digest(`hex`);
-    var headers = {};
-    headers[constant.checksumTokenHeader] = digestSha256;
-
-    let userRequest = sendRequest(constant.userPath, headers);
-    if (!userRequest) {
-      console.error("Reached max fail attempts.")
-      process.exit(1)
+  if (!authRequest) {
+    console.error("Reached max fail attempts.")
+    process.exit(1)
+  }
+  else {
+   let authorizationToken = authRequest.headers.get(constant.badsecTokenHeader);
+   let digestSha256 = crypto.createHash(`sha256`).update(authorizationToken + constant.userPath).digest(`hex`);
+   var headers = {};
+   headers[constant.checksumTokenHeader] = digestSha256;
+ 
+   let userRequest = await sendRequest(constant.userPath, headers);
+   if (!userRequest) {
+     console.error("Reached max fail attempts.")
+     process.exit(1)
+   }
+   else {
+     userRequest.text()
+       .then(
+         response => {
+           console.log(response);
+           process.exit(1);
+         }
+       )
     }
-    console.log(JSON.stringify(userRequest.responseText.split(`\n`)));
-    process.exit(0);
+  }
 }
 
 main();
